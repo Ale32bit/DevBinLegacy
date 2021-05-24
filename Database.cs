e1aa898854613c0605b0c04b656d2e739c6d1083
Alessandro <4512372+Ale32bit@users.noreply.github.com>
2021-05-24 23:35:15 +0200

    other changes

--- Database.cs
+++ Database.cs
@@ -4,6 +4,41 @@ using System.Collections.Generic;
 
 namespace DevBin {
     public class Database {
+        public const string CreateSQL = @"
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+CREATE TABLE IF NOT EXISTS `pastes` (
+  `id` varchar(8) NOT NULL,
+  `authorId` int(11) DEFAULT NULL,
+  `title` varchar(255) NOT NULL DEFAULT 'Unnamed paste',
+  `syntax` varchar(255) NOT NULL DEFAULT 'plaintext',
+  `exposure` tinyint(3) unsigned NOT NULL DEFAULT 0,
+  `timestamp` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updateTimestamp` timestamp NULL DEFAULT NULL,
+  `views` int(10) unsigned NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`),
+  KEY `Author` (`authorId`),
+  CONSTRAINT `Author` FOREIGN KEY (`authorId`) REFERENCES `users` (`id`) ON DELETE SET NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(63) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `Unique` (`username`,`email`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;";
         public string ConnectionString { get; set; }
         public Database(string connectionString) {
             ConnectionString = connectionString;
