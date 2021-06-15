61200ce066b29b47487927f3b20182dd20b75378
Alessandro <4512372+Ale32bit@users.noreply.github.com>
2021-06-15 18:33:12 +0200

    changes

---
+++ Pages/User/RedirectLogin.cshtml.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DevBin.Pages.User {
+    [IgnoreAntiforgeryToken(Order = 2000)]
+    public class RedirectLoginModel : PageModel {
+        public ActionResult OnGet([FromQuery] string token) {
+            if (string.IsNullOrEmpty(token)) {
+                return Redirect("/");
+            }
+
+            HttpContext.Response.Cookies.Append("devbin_session_token", token, new CookieOptions() {
+                HttpOnly = false,
+                IsEssential = true,
+            });
+
+            return Redirect("/");
+        }
+    }
+}
